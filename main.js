da3039dac1eae4e59f3561045f2b95c14b021c9f
Bethany Priode <bpriode@gmail.com>
2017-08-14 17:15:40 -0400

    turning in

--- main.js
+++ main.js
@@ -6,16 +6,21 @@ const chalk = require('chalk');
 
     console.log("It is the " + chalk.magenta.bold(moment().format("DDDo")) + " day of the year.");
 
-    console.log("It is " + chalk.cyan.bold(moment.duration().get('seconds')) + " seconds into the day.");
+
+    let now = moment().unix();
+    let midnight = (moment(0, "H").unix());
+    let answer = (now - midnight);
+    console.log("It is " + chalk.cyan.bold(answer) + " seconds into the day.");;
+
 
     if (moment().isDST() === true) {
       console.log("It " + chalk.green.bold("is ") + "during Daylight Savings Time.");
     }else{
-      console.log(("It " + chalk.green.bold("is ") + " not during Daylight Savings Time."));
+      console.log(("It " + chalk.green.bold("is ") + "not during Daylight Savings Time."));
     }
 
     if (moment().isLeapYear() === true) {
       console.log("It " + chalk.redBright.bold("is ") + " a leap year.");
     }else{
-      console.log("It " + chalk.redBright.bold("is not ") + " a leap year.");
+      console.log("It " + chalk.redBright.bold("is not") + " a leap year.");
     }
